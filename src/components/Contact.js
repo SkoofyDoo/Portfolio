b49beb94e4667a4487cdb7039608110d3c1e40f9
Evgeny Quest <evgenykvest@gmail.com>
2026-03-25 13:51:10 +0100

    removed TN

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -19,7 +19,7 @@ export default function Contact(){
                 <motion.div key={index} initial={{opacity: 0, y: 50}} whileInView={{opacity: 1, y: 0}} transition={{duration: 0.5, delay: index * 0.15}} className="bg-zinc-900 rounded-xl p-6">
                     <h3 className="text-white text-xl font-bold mb-3">{contact.city}</h3>
                     <p className="text-gray-400">{contact.email}</p>
-                    <p className="text-gray-400">Telefonnummer: 0159 06454927</p>
+                    
                     <div className="grid grid-cols-1">
                         <a className="text-blue-400 hover:text-blue-300" href={contact.linkedIn}>LinkedIn</a>
                         <a className="text-blue-400 hover:text-blue-300" href={contact.github}>GitHub</a>
